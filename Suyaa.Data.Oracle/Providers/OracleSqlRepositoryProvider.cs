d16bbc58a7e2e3099c1d2324e3e3dce63a8df14e
Uncle Zheng <inmount@live.cn>
2023-12-12 19:05:09 +0800

    修改：修复编译报错

--- Suyaa.Data.Oracle/Providers/OracleSqlRepositoryProvider.cs
+++ Suyaa.Data.Oracle/Providers/OracleSqlRepositoryProvider.cs
@@ -15,35 +15,17 @@ namespace Suyaa.Data.Oracle.Providers
         /// 获取数据库命令管理器
         /// </summary>
         /// <param name="work"></param>
-        /// <param name="sql"></param>
         /// <returns></returns>
-        public DbCommand GetDbCommand(IDbWork work, string sql)
+        public DbCommand GetDbCommand(IDbWork work)
         {
             if (work.Connection is OracleConnection dbc)
             {
-                var sqlCommand = new OracleCommand(sql, dbc);
+                var sqlCommand = new OracleCommand();
                 sqlCommand.CommandTimeout = 600;
+                sqlCommand.Connection = dbc;
                 return sqlCommand;
             }
             throw new TypeNotSupportedException(work.Connection.GetType());
-        }
-
-        /// <summary>
-        /// 获取数据库命令管理器
-        /// </summary>
-        /// <param name="work"></param>
-        /// <param name="sql"></param>
-        /// <param name="parameters"></param>
-        /// <returns></returns>
-        public DbCommand GetDbCommand(IDbWork work, string sql, DbParameters parameters)
-        {
-            var sqlCommand = GetDbCommand(work, sql);
-            sqlCommand.Parameters.Clear();
-            foreach (var param in parameters)
-            {
-                sqlCommand.Parameters.Add(new OracleParameter(":" + param.Key, param.Value));
-            }
-            return sqlCommand;
         }
 
         /// <summary>
@@ -55,7 +37,8 @@ namespace Suyaa.Data.Oracle.Providers
         public DataSet GetDataSet(IDbWork work, string sql)
         {
             DataSet dataSet = new DataSet();
-            using var sqlCommand = GetDbCommand(work, sql);
+            using var sqlCommand = GetDbCommand(work);
+            sqlCommand.CommandText = sql;
             var sqlDataAdapter = new OracleDataAdapter { SelectCommand = (OracleCommand)sqlCommand };
             sqlDataAdapter.Fill(dataSet);
             return dataSet;
@@ -70,7 +53,13 @@ namespace Suyaa.Data.Oracle.Providers
         public DataSet GetDataSet(IDbWork work, string sql, DbParameters parameters)
         {
             DataSet dataSet = new DataSet();
-            using var sqlCommand = GetDbCommand(work, sql, parameters);
+            using var sqlCommand = GetDbCommand(work);
+            sqlCommand.CommandText = sql;
+            sqlCommand.Parameters.Clear();
+            foreach (var param in parameters)
+            {
+                sqlCommand.Parameters.Add(new OracleParameter(":" + param.Key, param.Value));
+            }
             var sqlDataAdapter = new OracleDataAdapter { SelectCommand = (OracleCommand)sqlCommand };
             sqlDataAdapter.Fill(dataSet);
             return dataSet;
